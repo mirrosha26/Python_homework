1a86a798c9825ffbfdf6379dc52c1fbcd492e0cb
Mikhail Miroshnikov <106276818+mirrosha26@users.noreply.github.com>
2022-07-04 22:10:40 +0300

    Create new task from listing 2.15
    Еще один способ обработки ошибок

---
+++ 2_Basic_operations/Listing/l_2_15.py
@@ -0,0 +1,23 @@
+'''
+Еще один способ обработки ошибок
+'''
+print("Операции сос писком чисел: ")
+#Контролируемый код
+try:
+  nums = eval(input("Введите спсиок чисел: "))
+  print("Получено значение: "+ str(nums))
+  a = int(nums[0])
+  b = int(nums[3])
+  print(str(a) + "/" + str(b) + " = " + str(a/b))
+except ValueError:
+  print("ValueError: Ошибка при преобразовании!")
+except NameError:
+  print("NameError: неверный идентификатор!")
+except IndexError:
+  print("IndexError: неверный индекс элемента!")
+except SyntaxError:
+  print("SyntaxError:  невозможно вычислить выражение!")
+except ZeroDivisionError:
+  print("ZeroDivisionError: попытка деления на ноль!")
+except :
+  print("Что-то пошло не так!")
