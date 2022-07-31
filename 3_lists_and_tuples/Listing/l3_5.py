4c48e2282bc6d19ef1595f920ae2851fcfa87788
Mikhail Miroshnikov <mirrosha26@gmail.com>
2022-07-31 07:22:21 +0300

    Create new task from listing 3.5

---
+++ 3_lists_and_tuples/Listing/l3_5.py
@@ -0,0 +1,18 @@
+'''
+Умножение списков
+'''
+Alpha = 5*[0]
+print(Alpha)
+Bravo = (1,)
+print(Bravo)
+print(type(Bravo))
+Charlie = [1,2]*3
+print(Charlie)
+Delta = [[1,2]]*3
+print(Delta)
+Echo = 4*(1,[2,3])
+print(Echo)
+Foxtrot = ([1]*2)*3
+print(Foxtrot)
+Golf = ([1]*2,)*3
+print(Golf)
